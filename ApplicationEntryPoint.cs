16d3570b5f25e117240eb739b08de1dd22946a0e
jaewoo\jaewoo <jamicoder94@gmail.com>
2025-04-28 21:24:06 +0900

    Define custom application entrypoint

---
+++ ApplicationEntryPoint.cs
@@ -0,0 +1,16 @@
+﻿namespace JiraClient
+{
+    public class ApplicationEntryPoint
+    {
+        [STAThread]
+        public static void Main()
+        {
+            App app = new App();
+            app.InitializeComponent();
+
+            MainWindow mainWindow = new MainWindow();
+
+            app.Run(mainWindow);
+        }
+    }
+}
